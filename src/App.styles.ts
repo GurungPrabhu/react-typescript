5efb6fe3b09f7461a3c68cf29d7ad8ec1c00a718
unknown <prabhu.eversoft@gmail.com>
2021-02-06 22:33:59 +0545

    Question and answer added and some custom styling done

---
+++ src/App.styles.ts
@@ -0,0 +1,11 @@
+import styled, { createGlobalStyle } from 'styled-components'
+import BGImage from "../public/background.jpg"
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        height: 100%;
+    }
+    body {
+        background-color: green;
+    }
+`
---
+++ src/components/questionCard.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+import { AnswerObject } from "../App"
+
+type Props = {
+    question: string;
+    answer: string[];
+    callback: any;
+    userAnswer: AnswerObject | undefined;
+    questionNr: number;
+    totalQuestions: number;
+}
+
+const QuestionCard: React.FC<Props> = ({
+    question, answer, callback, userAnswer, questionNr, totalQuestions
+}) => (
+    <div>
+      <p className="number">
+        Question: {questionNr} / {totalQuestions}
+      </p> 
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <div>
+        {answer.map(answer => (
+          <div key={answer}>
+            <button disabled={!!userAnswer} value={answer} onClick={callback}>
+                <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </button> 
+          </div>
+        ))}
+      </div>
+    </div>
+)
+
+export default QuestionCard
